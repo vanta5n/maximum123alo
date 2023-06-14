5d24267bccb9326b8698d262310fdf7b8dd4d74d
pepsisoda <vanta5n@mail.ru>
2023-06-14 21:41:10 +0500

    initial

---
+++ git.py
@@ -0,0 +1,12 @@
+# def strcounter(string):
+#     simbols = {}
+#     for i in string:
+#         if i not in simbols.keys():
+#             simbols[i] = 1
+#         else:
+#             simbols[i] += 1
+#     return simbols
+#
+# print(strcounter('123hfghfghfeeeeeeekkk;'))
+
+# # bread
