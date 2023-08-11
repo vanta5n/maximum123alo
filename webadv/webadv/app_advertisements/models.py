8f4ee4aedc2aded46b423825a50dc7c27100910e
pepsisoda <vanta5n@mail.ru>
2023-08-11 19:04:34 +0500

    дз по 7 занятию не пон

--- webadv/webadv/app_advertisements/models.py
+++ webadv/webadv/app_advertisements/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Advertisement(models.Model):
+    title = models.CharField("заголовок", max_length=128)
+    description = models.TextField("описание")
+    price = models.DecimalField("цена", max_digits=10, decimal_places=2)
+    auction = models.BooleanField("торг", help_text="отметьте, если торг уместен")
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'advertisements'
+
+    def __str__(self):
+        return f'Advertisement(id={self.id}, title={self.title}, price={self.price})'
